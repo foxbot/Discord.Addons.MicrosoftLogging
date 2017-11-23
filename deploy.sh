b58df8e692d0fa89b12604b846bf127cdb6d632c
Christopher F <foxbot@protonmail.com>
2017-11-22 21:52:37 -0500

    [ci] include nupkgs in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-token=$1
+build=$1
+token=$2
 project="Discord.Addons.MicrosoftLogging/Discord.Addons.MicrosoftLogging.csproj"
 feed="https://www.myget.org/F/discord-net/api/v3/index.json"
 
-build=$(printf %05d $TRAVIS_BULID_NUMBER)
+build=$(printf %05d $build)
 dotnet pack $project -c "Release" -o "../artifacts" /p:BuildNumber="$build" /p:IsTagBuild="$TRAVIS_TAG"
-dotnet nuget push -s $feed -k $token
+dotnet nuget push ./artifcats/*.nupkg -s $feed -k $token
